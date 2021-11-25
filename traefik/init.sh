94b5336b478764ee42e3b354695d9d7fa63a909a
Kevin Wang <kevin@xorkevin.com>
2021-11-24 21:09:20 -0800

    Add docker network creation

--- traefik/init.sh
+++ traefik/init.sh
@@ -6,5 +6,16 @@ log2() {
   printf '[%s] %s\n' "$(date)" "$*" 1>&2
 }
 
-log2 "Mkdir dc.run/traefik"
-mkdir -p dc.run/traefik
+acmedir=dc.run/traefik/letsencrypt
+acmefile="$acmedir/acme.json"
+if [ ! -e "$acmefile" ]; then
+  log2 "No acme file at $acmefile"
+  mkdir -p "$acmedir"
+  mask=$(umask)
+  umask 077
+  touch "$acmefile"
+  umask "$mask"
+  log2 "Created acme file at $acmefile"
+else
+  log2 "ACME file already present at $acmefile"
+fi
