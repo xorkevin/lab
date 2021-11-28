3244c332e3ffe48f9fb30ffbd3554b3deef469d4
Kevin Wang <kevin@xorkevin.com>
2021-11-27 21:33:09 -0800

    Impl governor secrets file

--- governor/genenv.sh
+++ governor/genenv.sh
@@ -2,7 +2,49 @@
 
 set -e
 
+envfile=dc.anvil_out/governor/.env
+
 mask=$(umask)
 umask 077
-cat dc.run/env/*.env > dc.anvil_out/governor/.env
+cat dc.run/env/*.env > "$envfile"
+umask "$mask"
+
+etcdir=dc.anvil_out/governor/govetc
+secretsfile="$etcdir/secrets.yaml"
+
+mkdir -p "$etcdir"
+
+. "$envfile"
+
+mask=$(umask)
+umask 077
+cat <<EOF > "$secretsfile"
+data:
+  setupsecret:
+    secret: "${GOV_SETUP_SECRET}"
+  dbauth:
+    username: postgres
+    password: "${POSTGRES_PASSWORD}"
+  kvauth:
+    password: "${REDIS_PASSWORD}"
+  objauth:
+    username: admin
+    password: "${MINIO_SECRET}"
+  eventsauth:
+    password: "${NATS_TOKEN}"
+  mailauth:
+    username: "${GOV_MAIL_USERNAME}"
+    password: "${GOV_MAIL_PASSWORD}"
+  mailkey:
+    secrets:
+      - "${GOV_MAILKEY}"
+  tokensecret:
+    secret: "${GOV_TOKEN_SECRET}"
+  otpkey:
+    secrets:
+      - "${GOV_OTPKEY}"
+  rsakey:
+    secret: |
+EOF
+cat dc.run/env/govrsakey.key | sed 's/^/      /' >> "$secretsfile"
 umask "$mask"
--- governor/init.sh
+++ governor/init.sh
@@ -21,7 +21,7 @@ if [ ! -e "$pgpassenv" ]; then
   mask=$(umask)
   umask 077
   cat <<EOF > "$pgpassenv"
-POSTGRES_PASSWORD="$pgpass"
+POSTGRES_PASSWORD='$pgpass'
 EOF
   umask "$mask"
   log2 "Wrote postgres pass to $pgpassenv"
@@ -36,7 +36,7 @@ if [ ! -e "$redispassenv" ]; then
   mask=$(umask)
   umask 077
   cat <<EOF > "$redispassenv"
-REDIS_PASSWORD="$redispass"
+REDIS_PASSWORD='$redispass'
 EOF
   umask "$mask"
   log2 "Wrote redis pass to $redispassenv"
@@ -51,7 +51,7 @@ if [ ! -e "$miniosecretenv" ]; then
   mask=$(umask)
   umask 077
   cat <<EOF > "$miniosecretenv"
-MINIO_SECRET="$miniosecret"
+MINIO_SECRET='$miniosecret'
 EOF
   umask "$mask"
   log2 "Wrote minio secret to $miniosecretenv"
@@ -66,10 +66,83 @@ if [ ! -e "$natstokenenv" ]; then
   mask=$(umask)
   umask 077
   cat <<EOF > "$natstokenenv"
-NATS_TOKEN="$natstoken"
+NATS_TOKEN='$natstoken'
 EOF
   umask "$mask"
   log2 "Wrote nats token to $natstokenenv"
 else
   log2 "NATS token env already present at $natstokenenv"
+fi
+
+govsetupsecretenv="$envdir/govsetupsecret.env"
+if [ ! -e "$govsetupsecretenv" ]; then
+  log2 "No gov setup secret env at $govsetupsecretenv"
+  govsetupsecret=$(gen_pass 64)
+  mask=$(umask)
+  umask 077
+  cat <<EOF > "$govsetupsecretenv"
+GOV_SETUP_SECRET='$govsetupsecret'
+EOF
+  umask "$mask"
+  log2 "Wrote gov setup secret to $govsetupsecretenv"
+else
+  log2 "Gov setup secret env already present at $govsetupsecretenv"
+fi
+
+govmailkeyenv="$envdir/govmailkey.env"
+if [ ! -e "$govmailkeyenv" ]; then
+  log2 "No gov mailkey env at $govmailkeyenv"
+  govmailkey=$(gen_pass 32)
+  mask=$(umask)
+  umask 077
+  cat <<EOF > "$govmailkeyenv"
+GOV_MAILKEY='\$cc20p\$$govmailkey'
+EOF
+  umask "$mask"
+  log2 "Wrote gov mailkey to $govmailkeyenv"
+else
+  log2 "Gov mailkey env already present at $govmailkeyenv"
+fi
+
+govtokensecretenv="$envdir/govtokensecret.env"
+if [ ! -e "$govtokensecretenv" ]; then
+  log2 "No gov token secret env at $govtokensecretenv"
+  govtokensecret=$(gen_pass 64)
+  mask=$(umask)
+  umask 077
+  cat <<EOF > "$govtokensecretenv"
+GOV_TOKEN_SECRET='$govtokensecret'
+EOF
+  umask "$mask"
+  log2 "Wrote gov token secret to $govtokensecretenv"
+else
+  log2 "Gov token secret env already present at $govtokensecretenv"
+fi
+
+govotpkeyenv="$envdir/govotpkey.env"
+if [ ! -e "$govotpkeyenv" ]; then
+  log2 "No gov otpkey env at $govotpkeyenv"
+  govotpkey=$(gen_pass 32)
+  mask=$(umask)
+  umask 077
+  cat <<EOF > "$govotpkeyenv"
+GOV_OTPKEY='\$cc20p\$$govotpkey'
+EOF
+  umask "$mask"
+  log2 "Wrote gov otpkey to $govotpkeyenv"
+else
+  log2 "Gov otpkey env already present at $govotpkeyenv"
+fi
+
+govrsakeyfile="$envdir/govrsakey.key"
+if [ ! -e "$govrsakeyfile" ]; then
+  log2 "No gov rsa key file at $govrsakeyfile"
+  govrsakey=$(gen_pass 32)
+  mask=$(umask)
+  umask 077
+  openssl genpkey -algorithm RSA -pkeyopt "rsa_keygen_bits:4096" > "$govrsakeyfile"
+  umask "$mask"
+  log2 "Wrote gov rsa key to $govrsakeyfile"
+else
+  log2 "Gov rsa key file already present at $govrsakeyfile"
 fi
